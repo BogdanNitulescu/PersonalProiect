7a95959ea2e36b96e1323b9e4a3221fb690503b8
BogdanNitulescu <nitulescu.bogdanm@gmail.com>
2022-04-01 21:27:19 +0300

    3 commit

--- Lectia 7/Tema_07.py
+++ Lectia 7/Tema_07.py
@@ -1,2 +1,93 @@
-print('Hello World')
-test 2 
+#ABSTRACTION
+#Clasa abstracta FormaGeometrica
+#Contine un field PI=3.14
+#Contine o metoda abstracta aria
+#Contine o metoda a clasei descrie() - aceasta printeaza pe ecran ‘Cel mai probabil am colturi’
+
+
+from abc import ABC , abstractmethod
+
+class FormaGeometrica(ABC):
+    pi = 3.14
+
+    def descriere(self):
+        print('Cel mai probabil am colturi!')
+
+    @abstractmethod
+    def aria(self):
+        pass
+
+
+class Patrat(FormaGeometrica):
+
+    def descriere(self):
+        print('Cel mai probabil am colturi!')
+
+    def aria(self):
+        print(f'Aria Patrat = {self.__latura * self.__latura}')
+
+    def __init__(self,latura):
+        self.__latura = latura
+
+    @property
+    def latura(self):
+        return self.__latura
+
+    @latura.getter
+    def latura(self):
+        print(f'Getter: Latura este {self.__latura}')
+
+    @latura.setter
+    def latura(self,latura):
+        print(f'Setter: Noua latura este {latura}')
+        self.__latura = latura
+
+    @latura.deleter
+    def latura(self):
+        print(f'Deleter : Am sters latura')
+        self.__latura = None
+
+
+class Cerc(FormaGeometrica):
+    def descriere(self):
+        print('Cel mai probabil nu am colturi!')
+
+    def aria(self):
+        print(f'Aria cercului = {self.pi * self.__raza ** 2}')
+
+    def __init__(self,raza):
+        self.__raza = raza
+
+    @property
+    def raza(self):
+        return self.__raza
+
+    @raza.getter
+    def raza(self):
+        print(f'Getter:raza este {self.__raza}')
+
+    @raza.setter
+    def raza(self,raza):
+        print(f'Setter: Noua raza este {raza}')
+        self.__raza = raza
+
+    @raza.deleter
+    def raza(self):
+        print(f'Deleter : Am sters raza')
+        self.__raza = None
+
+
+
+forma = Patrat(3)
+forma.aria()
+forma.latura = 5
+forma.latura
+forma.aria()
+del forma.latura
+forma.latura
+
+raza = Cerc(2)
+raza.descriere()
+raza.aria()
+raza.raza = 10
+raza.raza
--- Lectia 7/Test.py
+++ Lectia 7/Test.py
@@ -1,1 +1,78 @@
-print('Test')
+# Creati o clasa Student cu 2 atribute, un nume si o nota.
+# Instantiati cele 2 fielduri prin intermediul constructorului.
+# a) Scrieti 2 functii pentru nota: get_grade care sa returneze nota studentului si set_grade care sa ii modifice nota.
+# Asigurativa ca e si logic, nu doar ca functioneaza:)!
+# b) Scrieti o functie care sa descrie elevul: "The student`s name is .... and has the grade ..."
+# c) Creati o lista de studenti primita ca input de la tastatura; dupa ce ati construit-o, afisati descrierea fiecarui student
+# d) Filtrati lista de studenti (sau construiti una noua) astfel incat sa ramana doar acei studenti care au o nota de trecere:
+# de preferat determinati daca studentul are nota de trecere sau nu folosindu`va de o alta functie a clasei Student
+
+class Student:
+    def __init__(self,nume,nota):
+       self.nume = nume
+       self.nota = nota
+
+    def get_grade(self):
+        return self.nota
+
+    def set_grade(self,nota_noua):
+        verificare = True
+        while verificare:
+            try:
+                nota_noua = int(nota_noua)
+                assert nota_noua >= 0 and nota_noua <= 10
+
+            except:
+                print('Invalid option')
+                nota_noua = input('Introduceti noua nota : ')
+
+            else:
+                verificare = False
+                self.nota = nota_noua
+
+    def student_profile(self):
+        print(f'The student`s name is {self.nume} and has the grade {self.nota}!')
+
+list_of_student = []
+while True:
+
+    student_name = input('Student name :')
+    if student_name == 'Stop':
+        break
+
+    student_nota = int(input('Nota Studentulu :'))
+    student1=Student(student_name,student_nota)
+    list_of_student.append(student1)
+
+
+
+for elem in list_of_student:
+    elem.student_profile()
+
+
+pass_student = []
+print('Elevi Admisi!')
+for elem in list_of_student:
+    if elem.get_grade() >= 5:
+        pass_student.append(elem)
+for elem in pass_student:
+    elem.student_profile()
+
+
+
+
+from  prettytable import PrettyTable
+
+myTable = PrettyTable(['Student Name','Class','Section'])
+myTable.add_row(['Bogdan','12','A'])
+myTable.add_row(['Ancuta','12','A'])
+myTable.add_row(['Cira','12','A'])
+myTable.add_row(['Florina','12','A'])
+myTable.add_row(['Mihai','12','A'])
+myTable.add_autoindex('sr.No')
+myTable.del_column('Class')
+
+
+print(myTable)
+
+
